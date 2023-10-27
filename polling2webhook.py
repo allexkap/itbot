450570b1375d37c6ea45e89b760c0ad5abaed651
allexkap <aekpstn@gmail.com>
2023-10-27 12:49:57 +0300

    add args parser

--- polling2webhook.py
+++ polling2webhook.py
@@ -1,22 +1,37 @@
 import json
 import os
+from argparse import ArgumentParser
 
 import requests
 
-TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+parser = ArgumentParser()
+parser.add_argument('-a', '--address', default='localhost')
+parser.add_argument('-p', '--port', default='8000')
+parser.add_argument('-s', '--https', action='store_true')
+parser.add_argument('-t', '--timeout', default='60')
+parser.add_argument('-w', '--webhook', default='telegram_bot/webhook/')
+args = parser.parse_args()
 
-url_tg = 'https://api.telegram.org/bot%s/getupdates'
-url_wh = 'http://localhost:8000/telegram_bot/webhook/'
+
+url_tg = 'https://api.telegram.org/bot{}/getupdates'.format(
+    os.getenv('TELEGRAM_BOT_TOKEN')
+)
+url_wh = '{}://{}:{}/{}'.format(
+    'https' if args.https else 'http',
+    args.address,
+    args.port,
+    args.webhook,
+)
 
 params = {
     'offset': 0,
     'limit': 1,
-    'timeout': 60,
+    'timeout': args.timeout,
 }
 
 
 while True:
-    response = requests.get(url_tg % TELEGRAM_BOT_TOKEN, json=params)
+    response = requests.get(url_tg, json=params)
     if not response.ok:
         print(response, response.content)
         break
